8d146f992af1a52bdc24f39f7b0a9e3d6d183068
antonovaekaterina <antonova.ekaterina19@yandex.ru>
2020-12-27 15:59:21 +0700

    add error boundary component

--- src/Application.tsx
+++ src/Application.tsx
@@ -2,14 +2,17 @@ import * as React from 'react';
 import Router from './components/Router';
 import Layout from './components/Layout';
 import routes from './pages';
+import ErrorBoundary from './components/ErrorBoundary';
 
 export default class Application extends React.PureComponent {
     render() {
         return (
-            <Router
-                layout={Layout}
-                routes={routes}
-            />
+            <ErrorBoundary>
+                <Router
+                    layout={Layout}
+                    routes={routes}
+                />
+            </ErrorBoundary>
         );
     }
 }
