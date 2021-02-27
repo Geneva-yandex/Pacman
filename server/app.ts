ab55c913574740270176ca7ae54f63c8bad824a1
Konstantin Terekhin <konstantin.teryohin@rubius.com>
2021-02-27 19:34:10 +0700

    changed port

--- server/app.ts
+++ server/app.ts
@@ -10,7 +10,7 @@ import routes from '../src/pages/index';
 
 const sequelizeOptions: SequelizeOptions = {
     host: 'postgres',
-    port: 5432,
+    port: 5433,
     username: 'postgres',
     password: 'newPassword',
     database: 'packman',
